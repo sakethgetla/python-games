4c7f485428004b03c767be6b51b74643ebc2f9cd
saketh <saketh.getla@gmail.com>
2019-06-25 11:48:34 +1000

    camera working

--- pyVis/pyCamera.py
+++ pyVis/pyCamera.py
@@ -1,12 +1,38 @@
-import cv2
 import numpy as np
+import cv2
 
 cap = cv2.VideoCapture(0)
-while True:
-    ret.img = cap.read()
-    cv2.imshow('video output', img)
-    k=cv2.waitKey(10) & 0xff
-    if k==27:
+
+while(True):
+    # Capture frame-by-frame
+    ret, frame = cap.read()
+
+    # Our operations on the frame come here
+    # display in gray
+    #gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    #cv2.imshow('frame', gray)
+
+    # Display the resulting frame
+    cv2.imshow('frame', frame)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
         break
+
+# When everything done, release the capture
 cap.release()
 cv2.destroyAllWindows()
+
+
+
+#import cv2
+
+#import numpy as np
+#
+#cap = cv2.VideoCapture(0)
+#while True:
+#    ret.img = cap.read()
+#    cv2.imshow('video output', img)
+#    k=cv2.waitKey(10) & 0xff
+#    if k==27:
+#        break
+#cap.release()
+#cv2.destroyAllWindows()
